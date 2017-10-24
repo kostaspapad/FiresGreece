c3eaa3f83165a4f1d01a4ad08951bdafbfbd6460
kostaspapad <kosPa@protonmail.com>
2017-10-25 02:29:33 +0300

    fix js

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,8 @@
 
 jQuery(document).ready(function($) {
+    
+    
     initMap();
-      // Get init map geo data
-      //getInitGeoData(myIcon);
-    //   for ( var i=0; i < markers.length; ++i )
-    //   {
-    //    L.marker( [markers[i].lat, markers[i].lng], {icon: myIcon} )
-    //     .bindPopup( '<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>' )
-    //     .addTo( map );
-    //   }
-
-
 
 
     $('.datepicker').datepicker();
